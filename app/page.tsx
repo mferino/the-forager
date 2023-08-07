46febd8f3dd7c7fc2b1f235ff7ad72f520e2d2f2
Michael Ference <michael@firstthought.io>
2023-08-07 15:55:40 -0500

    working on input stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,47 @@
-"use client"
+"use client";
 
 import React from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [value, setValue] = React.useState("");
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   React.useEffect(() => {
-    if (inputRef?.current) {
-      inputRef.current.focus();
+    const currentInputRef = inputRef.current;
+
+    if (currentInputRef) {
+      currentInputRef.focus();
+
+      const handleBlur = () => {
+        currentInputRef.focus();
+      }
+
+      currentInputRef.addEventListener("blur", handleBlur);
+
+      return () => {
+        currentInputRef.removeEventListener("blur", handleBlur);
+      }
     }
   }, [])
 
   async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const str = e.currentTarget.value;
     console.log(str, " :: str");
-    const res = await fetch('localhost:3000/api', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(str)
-    });
-    const maxStreak = await res.json();
-    console.log(maxStreak, " :: inputValues");
-    setValue(str);
+    // const res = await fetch("http://localhost:3000/api", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(str),
+    // });
+    // const maxStreak = await res.json();
+    // console.log(maxStreak, " :: inputValues");
+    console.log(inputRef?.current?.value, " :: inputRef.current");
   }
 
   return (
     <div className={styles.maxWidthWrapper}>
-      <div className={styles.header}>Hello</div>
-      <input value={value} onChange={handleChange} ref={inputRef} />
+      <input onChange={handleChange} ref={inputRef} />
     </div>
   );
 }
